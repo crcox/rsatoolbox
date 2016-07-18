afdd809c15ddb258d8156b7c821a444d7eac408e
Chris Cox <crcox@wisc.edu>
2016-07-18 11:56:25 -0400

    spacesToUnderscores

---
+++ +rsa/+util/spacesToUnderscores.m
@@ -0,0 +1,22 @@
+function stringOut = spacesToUnderscores(stringIn);
+%
+% removeSpaces will replace all spaces with underscores within a string
+%
+% Cai Wingfield 1-2010
+%__________________________________________________________________________
+% Copyright (C) 2010 Medical Research Council
+
+import rsa.*
+import rsa.fig.*
+import rsa.fmri.*
+import rsa.rdm.*
+import rsa.sim.*
+import rsa.spm.*
+import rsa.stat.*
+import rsa.util.*
+
+if ~isempty(stringIn)
+	stringOut = strrep(stringIn, ' ', '_');
+end
+
+end%function
